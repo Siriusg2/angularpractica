d6898f467daacd418f2dab9d1d8a54efbc3a5b93
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-06-06 14:51:05 -0500

    styles y templates inline

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-  templateUrl: './empleado.component.html',
+templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.scss']
+ // styles : ["p{background-color: red;}"],
+ // template:"<p>aqui va un empleado</p>",
 })
 export class EmpleadoComponent {
 
